48bafd4712c2fb6684133b9de9560898e2867606
lliutianc <lliuitanc@gmail.com>
2020-10-13 00:30:55 -0400

    framework for stage 1: tp

---
+++ data.py
@@ -0,0 +1,105 @@
+import re
+import csv
+
+import nltk
+from nltk.stem import *
+try:
+    from nltk.corpus import stopwords
+except:
+    nltk.download('stopwords')
+    from nltk.corpus import stopwords
+
+import numpy as np
+import torch.nn as nn
+from torch.utils.data import Dataset
+
+TRAIN_FILE = 'dataset/Train.csv'
+VALID_FILE = 'dataset/Valid.csv'
+TEST_FILE = 'dataset/Test.csv'
+
+stemmer = PorterStemmer()
+STOPS = set(stemmer.stem(w) for w in stopwords.words('english'))
+
+
+def is_stopword(word):
+    STOP_PAT = re.compile('^[a-zA-Z]{2,}$')
+    return not STOP_PAT.match(word) and word not in STOPS
+
+
+def data_file(data):
+    assert data in ['train', 'valid', 'test'], f'invalid dataset: {data}'
+    if data == 'train':
+        return TRAIN_FILE
+    if data == 'valid':
+        return VALID_FILE
+    if data == 'test':
+        return TEST_FILE
+
+
+class IMDBDataset(Dataset):
+    def __init__(self, data, data_limit=None, device='cpu'):
+        neg, pos = [], []
+
+        with open(data_file(data), 'r') as file:
+            reader = csv.reader(file)
+            next(reader)
+            for idx, line in enumerate(reader):
+                words = [stemmer.stem(word) for word in line[0].split() if not is_stopword(word)]
+                if line[1] == '1':
+                    pos.append(words)
+                else:
+                    neg.append(words)
+                if data_limit:
+                    if (idx + 1) >= data_limit:
+                        break
+
+        self.pos = pos
+        self.neg = neg
+
+        self.x = self.pos + self.neg
+        self.y = [1 for _ in range(len(pos))] + [0 for _ in range(len(neg))]
+
+    def __len__(self):
+        return len(self.x)
+
+    def __getitem__(self, i):
+        return self.x[i], self.y[i]
+
+    def shuffle(self):
+        order = np.arange(len(self))
+        np.random.shuffle(order)
+        self.x = self.x[order]
+        self.y = self.y[order]
+
+
+def load_LDA_data(model, dataset, include_llk=False):
+    lda_x = []
+    llk_x = []
+    for x, _ in dataset:
+        x = model.make_doc(x)
+        x, llk = model.infer(x)
+        lda_x.append(x)
+        llk_x.append(llk)
+
+    lda_x = np.array(lda_x)
+    llk_x = np.array(llk_x)
+    lda_y = np.array(dataset.y)
+
+    if include_llk is False:
+        return lda_x, lda_y
+    else:
+        return lda_x, llk_x, lda_y
+
+
+def load_LDA_data_batch(model, dataset, include_llk=False):
+    docs = [model.make_doc(x) for (x, _) in dataset]
+    lda_x, llk = model.infer(docs)
+
+    lda_x = np.array(lda_x)
+    llk = np.array(llk)
+    lda_y = np.array(dataset.y)
+
+    if include_llk is False:
+        return lda_x, lda_y
+    else:
+        return lda_x, llk, lda_y
---
+++ lda_trail.py
@@ -0,0 +1,137 @@
+import argparse, os, time
+
+import tomotopy as tp
+import numpy as np
+import matplotlib.pyplot as plt
+
+from data import *
+
+
+def tp_one_trail(dataset, model_type, topic_size, sample_size,
+             max_iter=1000, min_iter=None, checkpoint=None, stop_increase=10, metric='ll_word'):
+    assert model_type in ['lda', 'ctm'], f'invalid `model_type`: {model_type}...'
+    assert metric in ['ll_word', 'perplexity'], f'invalid `metric`: {metric}...'
+    if model_type == 'lda':
+        model = tp.LDAModel(k=topic_size)
+    if model_type == 'ctm':
+        model = tp.CTModel(k=topic_size)
+
+    sample_size = min(sample_size, len(dataset))
+    for i in range(sample_size):
+        doc, label = dataset[i]
+        model.add_doc(doc)
+
+    if min_iter is None:
+        min_iter = max_iter // 10
+    if checkpoint is None:
+        checkpoint = max_iter // 10
+
+    for i in range(min_iter):
+        model.train(i)
+
+    pre_metric = - np.infty
+    stop_increase_cnt = 0.
+    for i in range(0, max_iter, checkpoint):
+        model.train(i)
+        if metric == 'll_word':
+            cur_metric = model.ll_per_word
+        if metric == 'perplexity':
+            cur_metric = model.perplexity
+        if cur_metric >= pre_metric:
+            pre_metric = cur_metric
+        else:
+            stop_increase_cnt += 1
+
+        if stop_increase_cnt >= stop_increase:
+            break
+
+    return model
+
+
+def boxplot_results(results_data, results_title, figwidth=5, figheight=4):
+    result = np.array(results_data).T
+
+    plt.figure(figsize=(figwidth, figheight))
+    _ = plt.boxplot(result)
+    plt.xticks(range(1, len(results_data) + 1), results_title)
+    plt.savefig(plt_path)
+
+
+def eval_model(model, dataset, metric='ll'):
+    docs = [model.make_doc(x) for (x, _) in dataset]
+    lda_x, llk = model.infer(docs)
+    if metric == 'll':
+        return llk
+    if metric == 'perplexity':
+        # TODO: how to compute perplexity?
+        return llk
+
+
+def run_trails(args, choice_set):
+    start = time.time()
+    trainset = IMDBDataset('train')
+    print(f'Finish: prepare train set (size: {len(trainset)}) in {(time.time() - start):.3f} seconds.')
+
+    start = time.time()
+    validset = IMDBDataset('test')
+    print(f'Finish: prepare valid set (size: {len(validset)}) in {(time.time() - start):.3f} seconds.')
+
+    results_data = []
+    results_title = []
+    for (n, k) in choice_set:
+        if n > len(trainset):
+            continue
+        start = time.time()
+        cur_result = []
+
+        for _ in range(args.rep_times):
+            trained_model = tp_one_trail(trainset, args.model, k, n,
+                                         args.max_iter, args.min_iter, args.checkpoint,
+                                         args.stop_increase, args.metric)
+
+            cur_result.append(eval_model(trained_model, validset, args.metric))
+        cur_result = np.array(cur_result)
+        results_title.append(f'{n}&{k}')
+        results_data.append(cur_result.mean(0))
+
+        print(f'Finish: [{n}&{k}] trails in {(time.time() - start):.3f} seconds.')
+
+    boxplot_results(results_data, results_title)
+
+
+if __name__ == '__main__':
+    cur_path = os.path.abspath(os.path.dirname(__file__))
+    result_path = os.path.join(cur_path, 'result')
+
+    parser = argparse.ArgumentParser(description="IWAE experiment")
+    parser.add_argument("--model", type=str, choices=['lda', 'ctm'], default='lda')
+    parser.add_argument("--task", type=str, choices=['n', 'k', 'nk'], default='k')
+    parser.add_argument("--rep_times", type=int, default=1)
+    # train
+    parser.add_argument("--max_iter", type=int, default=1_000)
+    parser.add_argument("--min_iter", type=int, default=None)
+    parser.add_argument("--checkpoint", type=int, default=None)
+    parser.add_argument("--stop_increase", type=int, default=10)
+    parser.add_argument("--metric", type=str, default='ll_word')
+    parser.add_argument("--n", type=int, default=20_000)
+    parser.add_argument("--k", type=int, default=100)
+
+    args = parser.parse_args()
+
+    if args.task == 'n':
+        ns = [5_000, 10_000, 20_000, 40_000]
+#         ns = [500, 1000, 2000]
+        choice_set = [(n, args.k) for n in ns]
+    if args.task == 'k':
+        ks = [10, 50, 100, 300]
+        choice_set = [(args.n, k) for k in ks]
+    if args.task == 'nk':
+        choice_set = [(5_000, 10), (10_000, 50), (20_000, 100), (40_000, 300)]
+
+
+    plt_path = os.path.join(result_path, args.model, args.task)
+    start = time.time()
+    print('Start trails.')
+    run_trails(args, choice_set)
+    print(f"""Finish trails in {(time.time() - start):.3f} seconds, 
+            See result at {plt_path}...""")
