fd64af4b765b5e09d289978ae6b72374768dbe93
lliutianc <lliuitanc@gmail.com>
2020-10-14 10:27:25 -0400

    change task setting

--- task_ltc.sh
+++ task_ltc.sh
@@ -6,23 +6,23 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=32
-#SBATCH --mem-per-cpu=32000m 
+#SBATCH --mem=32000m 
 #SBATCH --time=10:00:00
 #SBATCH --account=stats_dept1
 #SBATCH --partition=standard
 #SBATCH --output=/home/%u/outputs/%x-%j.log
 
 # <include the code as if you are running it from the terminal>
-python3 -u lda_trial.py --model lda --task n --rep_times 5 --metric pp --max_iter 500 
+python3 -u lda_trial.py --model lda --task n --rep_times 3 --metric pp --max_iter 500 
 echo $'FINISH: LDA - n'
-python3 -u lda_trial.py --model lda --task k --rep_times 5 --metric pp --max_iter 500
+python3 -u lda_trial.py --model lda --task k --rep_times 3 --metric pp --max_iter 500
 echo $'FINISH: LDA - '
-python3 -u lda_trial.py --model lda --task nk --rep_times 5 --metric pp --max_iter 500
+python3 -u lda_trial.py --model lda --task nk --rep_times 3 --metric pp --max_iter 500
 echo $'FINISH: LDA - nk'
 
-python3 -u lda_trial.py --model ctm --task n --rep_times 5 --metric pp --max_iter 500 
+python3 -u lda_trial.py --model ctm --task n --rep_times 3 --metric pp --max_iter 500 
 echo $'FINISH: CTM - n'
-python3 -u lda_trial.py --model ctm --task k --rep_times 5 --metric pp --max_iter 500
+python3 -u lda_trial.py --model ctm --task k --rep_times 3 --metric pp --max_iter 500
 echo $'FINISH: CTM - k'
-python3 -u lda_trial.py --model ctm --task nk --rep_times 5 --metric pp --max_iter 500
+python3 -u lda_trial.py --model ctm --task nk --rep_times 3 --metric pp --max_iter 500
 echo $'FINISH: CTM - nk'
